71cc52f2fedef240880abc9bf422c36157f26433
joekingsley <joekingsleymukundi@gmail.com>
2022-06-27 21:10:38 +0300

    host test

--- customers/customers/urls.py
+++ customers/customers/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tenant/', include('tenant.urls')),
+    path('host/', include('host.urls')),
 ]
