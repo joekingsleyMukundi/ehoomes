62e37b6b18e02866d7595bf5980c0b6bd2323121
joekingsley <joekingsleymukundi@gmail.com>
2022-07-03 00:28:27 +0300

    add host producer rabbit mq

---
+++ customers/host/producer.py
@@ -0,0 +1,15 @@
+import json
+import pika
+from dotenv import load_dotenv
+
+load_dotenv()
+# package to help send events
+params = pika.URLParameters('amqps://nqsdtgdi:CBnX14b-56LW_49P7SFMRudTTAFg8keX@rat.rmq2.cloudamqp.com/nqsdtgdi')
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    print(body)
+    channel.basic_publish(exchange='', routing_key='auth', body=json.dumps(body), properties=properties)
