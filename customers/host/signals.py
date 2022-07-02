de531e596e5104c4f0d28b460c1765034f4a1a0b
joekingsley <joekingsleymukundi@gmail.com>
2022-07-03 00:26:16 +0300

    add host verification

--- customers/host/signals.py
+++ customers/host/signals.py
@@ -2,3 +2,6 @@ from django.db.models.signals import post_save
 from ..utils.sms import SMS
 
 def host_request(sender, instance, created, **kwargs):
+    if created:
+        host = instance
+        
--- customers/host/views.py
+++ customers/host/views.py
@@ -5,12 +5,15 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from utils.sms import SMS
-from ..tenant.models import *
-from ..errors.db_error import DbConnectionError
+from tenant.models import *
+from errors.db_error import *
+from decorators.custom_auth_decorators import authenticate_user
+from .models import *
 
 
 # Create your views here.
 @api_view(['GET', 'POST'])
+@authenticate_user
 def become_host(request):
     if request.method == 'POST':
         phone = request.data['phone']
@@ -28,5 +31,23 @@ def become_host(request):
 
 
 @api_view(['GET', 'POST'])
+@authenticate_user
+def activate_host(request):
+    if request.method == 'POST':
+        code = request.data['verification_code']
+        user = users_model.objects.get(id=request.user.id)
+        db_code = user.otp
+        if not code == db_code:
+            raise ValidationError('verification code is invalid')
+        try:
+            created_host = host_model.objects.create(host_id=user.user_id, host_name=username, host_email=email,
+                                                     host_phone=user_phone)
+            return Response({"success": True, "message": "host created  successfully"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            raise DbConnectionError(e)
+
+
+@api_view(['GET', 'POST'])
+@authenticate_user
 def host_dashboard(request):
     return Response("success", status=status.HTTP_200_OK)
